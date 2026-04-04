97c1a5380820bb8a14308324e9764b4769cf9962
zafiragithub <33481431+zafiragithub@users.noreply.github.com>
2026-04-05 05:45:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 // ==========================================
 
 // 1. URL MESIN UTAMA (GOOGLE APPS SCRIPT)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzsPe6bTlcpd5IiCN7UUM0z0bdFFF3HKQfrzTyB7qE1R3_Kfg9-d9AZKim_zp0LPhMo/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyW4nRHnI57knObTbQUXvu2ckV-eQEAxAt6XR3HIjlw7iBFZWf9aGIv-HjrlFFysMrW/exec";
 window.SCRIPT_URL = SCRIPT_URL; // Back-up untuk dibaca oleh frontend HTML
 
 // 2. ID SPREADSHEET MASTER (WAJIB UNTUK CLOUDFLARE WORKER CMS)
 // (Silakan ganti tulisan di bawah dengan ID Spreadsheet bos yang asli)
-const SHEET_ID = "1A-yZScOWktfMzLjFR2e7BD0s2dpM21vNjRsATBEACtM";
+const SHEET_ID = "1ZVw_4h0lsh6hs3naNn_HSuJJ0Ghw3O7Ki72ojeamaD0";
 window.SHEET_ID = SHEET_ID;
 
 
